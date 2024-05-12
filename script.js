05c094d283761dc088be16bff68e2cd296f09c8d
riccbarelli <99691825+rb7984@users.noreply.github.com>
2024-05-12 12:11:18 +0200

    Added comments to modal photos

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
-var modal = document.getElementById("myModal");
+// var modal = document.getElementById("myModal");
 
-var img = document.getElementById("myImg");
-var modalImg = document.getElementById("img01");
+// var img = document.getElementById("myImg");
+// var modalImg = document.getElementById("img01");
 
-function showImage(src) {
+// function showImage(src) {
+//   modal.style.display = "block";
+//   modalImg.src = src;
+// }
+
+// function closeModal() {
+//   modal.style.display = "none";
+// }
+
+// window.onclick = function (event) {
+//   if (event.target == modal) {
+//     closeModal();
+//   }
+// };
+
+function showImage(src, modalId) {
+  var modal = document.getElementById(modalId);
+  var modalImg = modal.querySelector('.modal-content');
   modal.style.display = "block";
   modalImg.src = src;
 }
 
-function closeModal() {
+function closeModal(modalId) {
+  var modal = document.getElementById(modalId);
   modal.style.display = "none";
 }
 
 window.onclick = function (event) {
-  if (event.target == modal) {
-    closeModal();
+  if (event.target.classList.contains('modal')) {
+      event.target.style.display = "none";
   }
 };
