ba9041704ecffd206a5b7a47a1f634e4c1f4ae51
Riccardo Barelli <RiccardoBarelli@zurus-mbp-2.zuru.local>
2024-05-15 10:29:26 +0200

    fixed order

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-// var modal = document.getElementById("myModal");
-
-// var img = document.getElementById("myImg");
-// var modalImg = document.getElementById("img01");
-
-// function showImage(src) {
-//   modal.style.display = "block";
-//   modalImg.src = src;
-// }
-
-// function closeModal() {
-//   modal.style.display = "none";
-// }
-
-// window.onclick = function (event) {
-//   if (event.target == modal) {
-//     closeModal();
-//   }
-// };
-
 function showImage(src, modalId) {
   var modal = document.getElementById(modalId);
   var modalImg = modal.querySelector('.modal-content');
